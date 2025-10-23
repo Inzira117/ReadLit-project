de52447e8cbd3efaee86e5c3373541ba242a60a5
Inzira <inzira1995@gmail.com>
2025-10-23 11:01:25 -0700

    Header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import Header from './Header/Header'
 
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
 
    
     </>
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,3 +1,5 @@
+import './Header.css'
+
 type HeaderProps = {
   title: string
 }
@@ -8,10 +10,10 @@ export default function Header() {
             <div className="header__container">
             <h1 className="header__title">ReadLit</h1>
             <nav className="header__nav">
-                <ul>
-                    <li><a href="/">Genres</a></li>
-                    <li><a href="/about">To Read</a></li>
-                    <li><a href="/contact">My Books</a></li>
+                <ul className="header__nav-list">
+                    <li className="header__nav-item"><a href="/">Genres</a></li>
+                    <li className="header__nav-item"><a href="/to-read">To Read</a></li>
+                    <li className="header__nav-item"><a href="/my-books">My Books</a></li>
                 </ul>
             </nav>
             </div>
