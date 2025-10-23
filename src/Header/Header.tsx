ce388158ee11b4938f39325041440e2f7e8d9bea
Inzira <inzira1995@gmail.com>
2025-10-23 10:54:45 -0700

    Create Header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,10 +2,19 @@ type HeaderProps = {
   title: string
 }
 
-export default function Header({ title }: HeaderProps) {
+export default function Header() {
     return (
-        <header>
-            <h1>{title}</h1>
+        <header className="header">
+            <div className="header__container">
+            <h1 className="header__title">ReadLit</h1>
+            <nav className="header__nav">
+                <ul>
+                    <li><a href="/">Genres</a></li>
+                    <li><a href="/about">To Read</a></li>
+                    <li><a href="/contact">My Books</a></li>
+                </ul>
+            </nav>
+            </div>
         </header>
     )
 }
