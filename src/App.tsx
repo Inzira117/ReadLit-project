8deca32abd7a46d411c027671632d7824ef7cac6
Inzira <inzira1995@gmail.com>
2025-10-25 12:18:43 -0700

    Installed Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 import Header from './Header/Header'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,19 +1,17 @@
-import './Header.css'
 
-type HeaderProps = {
-  title: string
-}
+import { Link } from "react-router-dom";
+
 
 export default function Header() {
     return (
-        <header className="header">
-            <div className="header__container">
-            <h1 className="header__title">ReadLit</h1>
-            <nav className="header__nav">
-                <ul className="header__nav-list">
-                    <li className="header__nav-item"><a href="/">Genres</a></li>
-                    <li className="header__nav-item"><a href="/to-read">To Read</a></li>
-                    <li className="header__nav-item"><a href="/my-books">My Books</a></li>
+        <header className="bg-dark text-white py-3 mb-4">
+            <div className="container d-flex justify-content-between align-items-center">
+            <h1 className="h3 mb-0">ReadLit</h1>
+            <nav>
+                <ul className="nav">
+                    <li className="nav-item"><Link to="/" className="nav-link text-white">Genres</Link></li>
+                    <li className="nav-item"><Link to="/to-read" className="nav-link text-white">To Read</Link></li>
+                    <li className="nav-item"><Link to="/my-books" className="nav-link text-white">My Books</Link></li>
                 </ul>
             </nav>
             </div>
