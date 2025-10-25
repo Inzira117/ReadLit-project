53e1b4663c762fc460b074c6a8917b6423535ec7
Inzira <inzira1995@gmail.com>
2025-10-25 13:04:28 -0700

    Working on styles

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,25 +1,4 @@
 
-// import { Link } from "react-router-dom";
-
-
-// export default function Header() {
-//     return (
-//         <header className="container-fluid bg-dark text-white py-3 mb-4">
-//             <div className="container-fluid d-flex justify-content-between align-items-center">
-//             <h1 className="h3 mb-0">ReadLit</h1>
-//             <nav>
-//                 <ul className="nav">
-//                     <li className="nav-item"><Link to="/" className="nav-link text-white">Genres</Link></li>
-//                     <li className="nav-item"><Link to="/to-read" className="nav-link text-white">To Read</Link></li>
-//                     <li className="nav-item"><Link to="/my-books" className="nav-link text-white">My Books</Link></li>
-//                 </ul>
-//             </nav>
-//             </div>
-//         </header>
-//     )
-// }
-
-
 import { Link } from "react-router-dom";
 
 export default function Header() {
