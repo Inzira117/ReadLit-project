089c2145de911e572b631495620063a402b49dfb
Inzira <inzira1995@gmail.com>
2025-10-27 14:27:03 -0700

    Added quotes

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -3,6 +3,8 @@ export default function Main() {
         <main className="main">
             <h1 style={{ color: "rgb(250, 229, 204)", margin: "0 auto" }}>Welcome to ReadLit</h1>
             <h3 style={{ color: "rgb(250, 229, 204)" }}>Your one-stop solution for managing your reading list.</h3>
+            <p className="text-light">“Books are the mirrors of the soul.” <span>―Virginia Woolf</span></p>
+            <p className="text-light">“A reader lives a thousand lives before he dies. The man who never reads lives only one.” <span>— George R.R. Martin</span></p>
         </main>
     )
 }
